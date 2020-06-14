b321253087f2d4557b6b191a06fbba288762fcae
unknown <tereza.raidlova@gmail.com>
2020-06-14 08:54:03 +0200

    sharps/flats selector

--- main.js
+++ main.js
@@ -1,20 +1,37 @@
 const root = document.documentElement;
 const fretboard = document.querySelector(".fretboard");
-const numberOfFrets = 15;
-const numberOfStrings = 6;
+const selectedInstrumentSelector = document.querySelector('#instrument-selector');
+
+const accidentalSelector = document.querySelector('.accidental-selector');
+
+let numberOfFrets = 15;
+
 const singleFretMarkPositions = [3, 5, 7, 9, 15, 17, 19, 21];
 const doubleFretMarkPositions = [12, 24];
 const notesFlat = ['C','Dd','D','Eb','E','F','Gb','G','Ab','A','Bb','B'];
 const notesSharp = ['C','C#','D','D#','E','F','F#','G','G#','A','A#','B'];
 let accidentals = 'flats';
-const guitarTuning = [4, 11, 7, 2, 9, 4]
+
+const instrumentTuningPresets = {
+    'Guitar': [4, 11, 7, 2, 9, 4],
+    'Bass 4 strings': [7, 2, 9, 4],
+    'Bass 5 strings': [7, 2, 9, 4, 11],
+    'Ukulele': [9, 4, 0, 7]
+};
+
+let selectedInstrument = 'Guitar';
+let numberOfStrings = instrumentTuningPresets[selectedInstrument].length;
 
 const app = {
     init() {
-        this.setupFreatboard()
+        this.setupFreatboard();
+        this.setupSelectedInstrumentSelector();
+        this.setupEventListers();
     },
 
     setupFreatboard() {
+        fretboard.innerHTML = '';
+
         //updating css variables
         root.style.setProperty("--number-of-strings", numberOfStrings)
 
@@ -31,9 +48,9 @@ const app = {
                 string.appendChild(noteFret);
 
                 //adding note names to fretboard
-                // using the iterated fret and index of guitar tuning as note index
+                // using the iterated fret and index of tuning as note index
                 // the remainder of note index
-                let noteName = this.generateNoteNames((fret + guitarTuning[i]), accidentals);
+                let noteName = this.generateNoteNames((fret + instrumentTuningPresets[selectedInstrument][i]), accidentals);
                 noteFret.setAttribute('data-note', noteName)
                  
                 
@@ -48,12 +65,10 @@ const app = {
                     doubleFretMark.classList.add("double-fretmark");
                     noteFret.appendChild(doubleFretMark)
                 }
-                
             }
         }
     },
     generateNoteNames(noteIndex, accidentals) {
-        
         noteIndex = noteIndex % 12;
         let noteName;
         if (accidentals === 'flats') {
@@ -62,6 +77,36 @@ const app = {
             noteName = notesSharp[noteIndex]
         }
         return noteName;
+    },
+    setupSelectedInstrumentSelector(){
+        for(instrument in instrumentTuningPresets){
+            let instrumentOption = tools.createElement('option', instrument);
+            selectedInstrumentSelector.appendChild(instrumentOption);
+        }
+    },
+    setupEventListers(){
+        fretboard.addEventListener("mouseover", event =>{
+            if(event.target.classList.contains('note-fret')){
+                event.target.style.setProperty("--noteDotOpacity", 1)
+            }
+        });
+        fretboard.addEventListener("mouseout", (event)=>{
+            event.target.style.setProperty("--noteDotOpacity", 0)
+        });
+        selectedInstrumentSelector.addEventListener('change', (e)=>{
+            selectedInstrument = e.target.value;
+            numberOfStrings = instrumentTuningPresets[selectedInstrument].length;
+            this.setupFreatboard();
+        });
+        accidentalSelector.addEventListener('click', (event)=>{
+            if(event.target.classList.contains('acc-select')){
+                accidentals = event.target.value;
+                this.setupFreatboard();
+            } else {
+                return;
+            }
+        })
+
     }
 }
 
